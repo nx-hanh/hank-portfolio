85fb8939aa1fc95a361c3da5140045e2878267d9
nx-hanh <nx.hanh.personal@gmail.com>
2025-09-22 14:29:19 +0700

    chore: update dependencies and refactor component imports

--- v2/src/lib/fonts.ts
+++ v2/src/lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, JetBrains_Mono, Bungee } from 'next/font/google';
+import { Bungee, Inter, JetBrains_Mono } from 'next/font/google';
 import localFont from 'next/font/local';
 
 export const fontSans = Inter({
